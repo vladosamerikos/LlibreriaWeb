2221470e997ceca896e11da24bd07ef42fcce7ef
AlbertGirones <agirones600@gmail.com>
2022-10-31 18:25:50 +0100

    Optimizar admin.php

--- models/admin.php
+++ models/admin.php
@@ -1,45 +1,10 @@
 <?php
 require_once("database.php");
 class Admin extends Database {
+    
     private $email;
     private $nombre;
-    private $clave;
-
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getNombre()
-    {
-        return $this->nombre;
-    }
-
-    public function setNombre($nombre)
-    {
-        $this->nombre = $nombre;
-
-        return $this;
-    }
-
-    public function getClave()
-    {
-        return $this->clave;
-    }
-
-    public function setClave($clave)
-    {
-        $this->clave = $clave;
-
-        return $this;
-    }
+    private $clave; 
 
     public function login($email, $password){
         $consulta = $this->db->prepare("SELECT * FROM admin WHERE correo LIKE '$email' and clave LIKE '$password'");
