b2a7d0a595580bc4469b909a9343f49630d14191
vladosamerikos <vladosamerikos@gmail.com>
2022-10-31 14:11:54 +0100

    fix Index.php

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,35 +2,33 @@
 session_start();
 require "models/admin.php";
 
-class LoginController {
+class LoginController
+{
 
-    public function index() {
-        if(isset($_SESSION['email'])){
-            require "views/loginAdmin/formulario.php";
-        }else{
-            echo "hay session";
-        }
-        
+    public function index()
+    {
+        // if(isset($_SESSION['email'])){
+        require "views/loginAdmin/formulario.php";
+        // }else{
+        //     echo "hay session";
+        // }
+
     }
-    
-    public function login(){
+
+    public function login()
+    {
         $admin = new Admin();
         $_email = trim($_POST['email']);
         $_password = md5(trim($_POST['password']));
-        
+
         $_result = $admin->login($_email, $_password);
-        if ($_result){
+        if ($_result) {
             $_SESSION['email'] = $_email;
             echo "login correcto";
             header('Location: index.php?controller=Panel&action=mostrarLibros');
             die();
-        }else{
+        } else {
             echo "login incorrecto";
         }
-
-
     }
-
-
-
 }
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-
 <!--Controlador frontal: fichero que se encarga de cargarlo absolutamente todo  hola gerard-->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,36 +9,35 @@
     <link rel="stylesheet" href="style.css">
     <title>Llibreria Time2Read</title>
 </head>
+
 <body>
-<?php
-require_once "views/general/cabecera.html";
-require_once "views/general/menu.php";
-require_once "controllers/LoginController.php";
-require_once "controllers/PanelController.php";
+    <?php
+    require_once "views/general/cabecera.html";
+    require_once "views/general/menu.php";
+    require_once "controllers/LoginController.php";
+    require_once "controllers/PanelController.php";
+    require_once "controllers/SessionController.php";
 
-if (isset($_GET['controller'])){
-    $nombreController = $_GET['controller']."Controller";
-}
-else{
-    //Controlador per dedecte
-    $nombreController = "UsuarioController";
-}
-if (class_exists($nombreController)){
-    $controlador = new $nombreController(); 
-    if(isset($_GET['action'])){
-        $action = $_GET['action'];
+    if (isset($_GET['controller'])) {
+        $nombreController = $_GET['controller'] . "Controller";
+    } else {
+        //Controlador per dedecte
+        $nombreController = "LoginController";
     }
-    else{
-        $action ="mostrarTodos";
-    }
-    $controlador->$action();   
-}else{
+    if (class_exists($nombreController)) {
+        $controlador = new $nombreController();
+        if (isset($_GET['action'])) {
+            $action = $_GET['action'];
+        } else {
+            $action = "index";
+        }
+        $controlador->$action();
+    } else {
 
-    echo "No existe el controlador";
-}
-// require_once "views/general/pie.html";
-?>
+        echo "No existe el controlador";
+    }
+    // require_once "views/general/pie.html";
+    ?>
 </body>
+
 </html>
-
-
