261b827138578852f67cbd1271ca7132f89a619e
vladosamerikos <vladosamerikos@gmail.com>
2022-11-04 10:57:10 +0100

    add edit state pedido

--- views/adminPanel/tablaPedidos.php
+++ views/adminPanel/tablaPedidos.php
@@ -18,7 +18,14 @@ foreach ($catalogo as $pedido) {
         <td class='text'>" . $pedido['fk_id_usuario'] . "</td>
         <td class='text'>" . $pedido['fecha'] . "</td>
         <td class='text'>" . $pedido['total'] . "</td>
-        <td class='text'>" . $pedido['estado'] . "</td>
+        <td class='text'><form class='admin-panel-pedido-editar-estado-form' action='#'>
+            <select name='estado' id='estado' value='".$pedido['estado']."'>
+                <option value='pendiente'>Pendiente</option>
+                <option value='enviado'>Enviado</option>
+                <option value='entregado'>Entregado</option>
+            </select>
+            <input class='admin-panel-pedido-submit-button' type='image' name='submit' src='./img/upload2.svg' alt='Confirmar'>
+        </form></td>
         </tr>";
 }
 echo "</table>";
