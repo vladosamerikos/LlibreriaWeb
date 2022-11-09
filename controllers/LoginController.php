608f7ed0e296f513fe50d25f03d2317a46de217e
vladosamerikos <vladosamerikos@gmail.com>
2022-11-09 10:00:52 +0100

    Add login user

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,19 +1,28 @@
 <?php
 require "models/admin.php";
+require "models/usuario.php";
 
 class LoginController
 {
 
     public function index()
     {
-        if (isset($_SESSION['email'])) {
+        if (isset($_SESSION['email']) && $_SESSION['role']=='admin') {
             header('Location: index.php?controller=Libro&action=mostrarLibros');
+        }else if (isset($_SESSION['email']) && $_SESSION['role']=='user') {
+            header('Location: index.php?controller=Principal&action=mostrarPrincipal');
         } else {
-            require "views/loginAdmin/formulario.php";
+            require "views/loginUser/formulario.php";
         }
     }
 
-    public function login()
+    public function mostrarLoginAdmin(){
+        require "views/loginAdmin/formulario.php";
+    }
+
+
+
+    public function loginAdmin()
     {
         $admin = new Admin();
         $_email = trim($_POST['email']);
@@ -22,6 +31,7 @@ class LoginController
         $_result = $admin->login($_email, $_password);
         if ($_result) {
             $_SESSION['email'] = $_email;
+            $_SESSION['role']= 'admin';
             echo "login correcto";
             header('Location: index.php?controller=Libro&action=mostrarLibros');
             die();
@@ -29,4 +39,25 @@ class LoginController
             echo "login incorrecto";
         }
     }
+
+    public function loginUser()
+    {
+        $admin = new Usuario();
+        $_email = trim($_POST['email']);
+        $_password = md5(trim($_POST['password']));
+
+        $_result = $admin->login($_email, $_password);
+        if ($_result) {
+            $_SESSION['email'] = $_email;
+            $_SESSION['role']= 'user';
+            echo "login correcto";
+            // header('Location: index.php?controller=&action=');
+            // die();
+        } else {
+            echo "login incorrecto";
+        }
+    }
+
+
+
 }
