d40a5e3da4b07cd63e7fdd676ba10e1e57da74af
vladosamerikos <vladosamerikos@gmail.com>
2022-11-30 10:22:12 +0100

    Moree cesta styles

--- views/cesta/cesta.php
+++ views/cesta/cesta.php
@@ -3,24 +3,29 @@
 echo "<div class='cesta-container'>";
     foreach ($_SESSION['Cesta'] as $id=>$data){
         echo "<div class='cesta-item'>
-            <a class='cesta-item-imagen'><img class='cesta-imagen' src='".$data['imagen']."' alt='imagen producto'></a>
+            
             <div class='cesta-item-info-container'>
-                <p class='cesta-item-titulo'>".$data['titulo']."</p>
-                <p class='cesta-item-pecio'>".$data['precio']."</p>
+                <a href='index.php?controller=Libro&action=mostrarLibro&id=".$id."' class='cesta-item-imagen'><img class='cesta-imagen' src='".$data['imagen']."' alt='imagen producto'></a>
+                <div class='cesta-item-info-text-container'>
+                    <a href='index.php?controller=Libro&action=mostrarLibro&id=".$id."' class='cesta-item-titulo'>".$data['titulo']."</a>
+                    <p class='cesta-item-pecio'>".formatarPrecio($data['precio'])."€</p>
+                </div>
             </div>
             <div class='cesta-item-action-container'>
-                    <a class='cesta-item-action-container-item'><img class='cesta-item-action-img' src='./img/trash-bin.svg'></a>
+                    <a class='cesta-item-action-container-item cesta-item-action-container-item-basura'><img class='cesta-item-action-img' src='./img/trash-bin.svg'></a>
                     <div class='cesta-item-action-container-items'>
-                        <a class='cesta-item-action-container-item'><img class='cesta-item-action-img' src='./img/remove.svg'></a>
-                        <a class='cesta-item-action-container-item'>".$data['cant']."</a>
-                        <a class='cesta-item-action-container-item'><img class='cesta-item-action-img' src='./img/add.svg'></a>
+                        <a class='cesta-item-action-container-item cesta-item-action-container-item-dell'><img class='cesta-item-action-img' src='./img/remove.svg'></a>
+                        <a class='cesta-item-action-container-item cesta-item-action-container-item-num'>".$data['cant']."</a>
+                        <a class='cesta-item-action-container-item cesta-item-action-container-item-add'><img class='cesta-item-action-img' src='./img/add.svg'></a>
                     </div>
             </div> 
         </div>";
     }
-echo "</div>";
+echo "<div class='cesta-actions-container'>
+        <a class='cesta-action' href='index.php?controller=Cesta&action=limpiarCesta'><img class='cesta-item-action-img' src='./img/trash-bin.svg'>&nbsp Vaciar cesta</a>
+        <a class='cesta-action' href='index.php?controller=Principal&action=mostrarPaginaPrincipal'>Seguir comprando</a>
+    </div>
+</div>";
 
 
 
-
-echo "<a href='index.php?controller=Cesta&action=limpiarCesta'>Limpiar Cesta</a>";
