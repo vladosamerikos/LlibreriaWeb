92b9d1b221d3ab336323bb1ac7187ea51e287ff2
AlbertGirones <agirones600@gmail.com>
2022-12-02 09:53:38 +0100

    Seguridad libro, pedidos panel administrador

--- controllers/LibroController.php
+++ controllers/LibroController.php
@@ -7,46 +7,61 @@ class LibroController
 
     public function mostrarLibros()
     {
-        require_once "views/adminPanel/menu.php";
-        $libro = new Libro();
-        $catalogo = $libro->obtenerCatalogo();
-        require_once "views/adminPanel/tablaLibros.php";
+        if (isset($_SESSION['email']) && $_SESSION['role'] == 'admin'){
+            require_once "views/adminPanel/menu.php";
+            $libro = new Libro();
+            $catalogo = $libro->obtenerCatalogo();
+            require_once "views/adminPanel/tablaLibros.php";
+        }
+        else{
+            adminIncorrecte();
+        }
     }
 
     public function mostrarAnadirLibro()
     {
-        $categoria = new Categoria();
-        $listadoCategorias = $categoria->obtenerListadoActivos();
-        require_once "views/adminPanel/menu.php";
-        require_once "views/adminPanel/anadirLibro.php";
+        if (isset($_SESSION['email']) && $_SESSION['role'] == 'admin'){
+            $categoria = new Categoria();
+            $listadoCategorias = $categoria->obtenerListadoActivos();
+            require_once "views/adminPanel/menu.php";
+            require_once "views/adminPanel/anadirLibro.php";
+        }
+        else{
+            adminIncorrecte();
+        }
     }
 
     public function anadirLibro()
     {
-        require_once "views/adminPanel/menu.php";
-        $libro = new Libro();
-        $_idgenero = trim($_POST['idgenero']);
-        $_isbn = trim($_POST['isbn']);
-        $_nombre = $_POST['nombre'];
-        $_descripcion_short = $_POST['descripcion_short'];
-        $_descripcion = $_POST['descripcion'];
-        $_stock = $_POST['stock'];
-        $_precio_venta = $_POST['precio_venta'];
+        if (isset($_SESSION['email']) && $_SESSION['role'] == 'admin'){
+            require_once "views/adminPanel/menu.php";
+            $libro = new Libro();
+            $_idgenero = trim($_POST['idgenero']);
+            $_isbn = trim($_POST['isbn']);
+            $_nombre = $_POST['nombre'];
+            $_descripcion_short = $_POST['descripcion_short'];
+            $_descripcion = $_POST['descripcion'];
+            $_stock = $_POST['stock'];
+            $_precio_venta = $_POST['precio_venta'];
 
-        if (is_uploaded_file($_FILES['imagen']['tmp_name'])) {
-            $nombreDirectorio = "img/libros";
-            $idUnico = $_isbn;
-            $nomorig = $_FILES['imagen']['name'];
-            $cont = explode(".", $nomorig);
-            $ext = $cont[1];
-            $nombreFichero = $idUnico . "." . $ext;
-            move_uploaded_file(
-                $_FILES['imagen']['tmp_name'],
-                $nombreDirectorio . $nombreFichero
-            );
+            if (is_uploaded_file($_FILES['imagen']['tmp_name'])) {
+                $nombreDirectorio = "img/libros";
+                $idUnico = $_isbn;
+                $nomorig = $_FILES['imagen']['name'];
+                $cont = explode(".", $nomorig);
+                $ext = $cont[1];
+                $nombreFichero = $idUnico . "." . $ext;
+                move_uploaded_file(
+                    $_FILES['imagen']['tmp_name'],
+                    $nombreDirectorio . $nombreFichero
+                );
+            }
+            $_imagen = $nombreDirectorio . $nombreFichero;
+            $libro->anadir($_idgenero, $_isbn, $_nombre, $_descripcion_short, $_descripcion, $_stock, $_precio_venta, $_imagen);
         }
-        $_imagen = $nombreDirectorio . $nombreFichero;
-        $libro->anadir($_idgenero, $_isbn, $_nombre, $_descripcion_short, $_descripcion, $_stock, $_precio_venta, $_imagen);
+        else{
+            adminIncorrecte();
+        }
     }
 
     public function activarLibro()
@@ -67,53 +82,64 @@ class LibroController
 
     public function mostrarEditarLibro()
     {
-        $categoria = new Categoria();
-        $listadoCategorias = $categoria->obtenerListadoActivos();
-        $_idlibro = $_GET['id'];
-        $libro = new Libro();
-        $datosLibro = $libro->obtenerInfo($_idlibro);
-        require_once "views/adminPanel/menu.php";
-        require_once "views/adminPanel/editarLibro.php";
+        if (isset($_SESSION['email']) && $_SESSION['role'] == 'admin'){
+            $categoria = new Categoria();
+            $listadoCategorias = $categoria->obtenerListadoActivos();
+            $_idlibro = $_GET['id'];
+            $libro = new Libro();
+            $datosLibro = $libro->obtenerInfo($_idlibro);
+            require_once "views/adminPanel/menu.php";
+            require_once "views/adminPanel/editarLibro.php";
+        }
+        else{
+            adminIncorrecte();
+        }
     }
 
     public function editarLibro()
     {
-        require_once "views/adminPanel/menu.php";
-        $libro = new Libro();
-        $_oldimagen = $_POST['oldimagen'];
-        $_id_articulo = $_POST['id_articulo'];
-        $_idgenero = trim($_POST['idgenero']);
-        $_isbn = trim($_POST['isbn']);
-        $_nombre = $_POST['nombre'];
-        $_descripcion_short = $_POST['descripcion_short'];
-        $_descripcion = $_POST['descripcion'];
-        $_stock = $_POST['stock'];
-        $_precio_venta = $_POST['precio_venta'];
+        if (isset($_SESSION['email']) && $_SESSION['role'] == 'admin'){
+            require_once "views/adminPanel/menu.php";
+            $libro = new Libro();
+            $_oldimagen = $_POST['oldimagen'];
+            $_id_articulo = $_POST['id_articulo'];
+            $_idgenero = trim($_POST['idgenero']);
+            $_isbn = trim($_POST['isbn']);
+            $_nombre = $_POST['nombre'];
+            $_descripcion_short = $_POST['descripcion_short'];
+            $_descripcion = $_POST['descripcion'];
+            $_stock = $_POST['stock'];
+            $_precio_venta = $_POST['precio_venta'];
 
-        if (is_uploaded_file($_FILES['imagen']['tmp_name'])) {
-            // Eliminamos la imagen anterior
-            if (unlink($_oldimagen)) {
-                // mensaje que confirma la eliminacion
+            if (is_uploaded_file($_FILES['imagen']['tmp_name'])) {
+                // Eliminamos la imagen anterior
+                if (unlink($_oldimagen)) {
+                    // mensaje que confirma la eliminacion
+                } else {
+                    // mensaje que muestra error
+                }
+
+                $nombreDirectorio = "img/libros/";
+                $idUnico = $_isbn;
+                $nomorig = $_FILES['imagen']['name'];
+                $cont = explode(".", $nomorig);
+                $ext = $cont[1];
+                $nombreFichero = $idUnico . "." . $ext;
+                move_uploaded_file(
+                    $_FILES['imagen']['tmp_name'],
+                    $nombreDirectorio . $nombreFichero
+                );
+                $_imagen = $nombreDirectorio . $nombreFichero;
             } else {
-                // mensaje que muestra error
+                $_imagen = $_oldimagen;
             }
-
-            $nombreDirectorio = "img/libros/";
-            $idUnico = $_isbn;
-            $nomorig = $_FILES['imagen']['name'];
-            $cont = explode(".", $nomorig);
-            $ext = $cont[1];
-            $nombreFichero = $idUnico . "." . $ext;
-            move_uploaded_file(
-                $_FILES['imagen']['tmp_name'],
-                $nombreDirectorio . $nombreFichero
-            );
-            $_imagen = $nombreDirectorio . $nombreFichero;
-        } else {
-            $_imagen = $_oldimagen;
+            $libro->editar($_id_articulo, $_idgenero, $_isbn, $_nombre, $_descripcion_short, $_descripcion, $_stock, $_precio_venta, $_imagen);
+            header("Location: index.php?controller=Libro&action=mostrarLibros");
         }
-        $libro->editar($_id_articulo, $_idgenero, $_isbn, $_nombre, $_descripcion_short, $_descripcion, $_stock, $_precio_venta, $_imagen);
-        header("Location: index.php?controller=Libro&action=mostrarLibros");
+        else{
+            adminIncorrecte();
+        }
+        
     }
 
     public function mostrarLibro()
