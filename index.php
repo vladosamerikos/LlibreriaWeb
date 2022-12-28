8f621fb00e9218d93174dbe43b1c0e7f8d9aa9b5
VLad.Pasichnyk <vlad.pasichnyk@bitgenoma.com>
2022-12-28 17:25:39 +0100

    add favicon

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon.png">
     <title>Llibreria Time2Read</title>
 </head>
 
