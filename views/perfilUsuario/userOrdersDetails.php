7026997b7f24676bdaa0fa1d2803458e7403a5c3
AlbertGirones <agirones600@gmail.com>
2023-01-10 21:53:14 +0100

    Pedidos del usuario - Pagina responsive

--- views/perfilUsuario/userOrdersDetails.php
+++ views/perfilUsuario/userOrdersDetails.php
@@ -1,32 +1,18 @@
 <?php
-echo "
-<div class='profile-orders-panel-content-container'>
-<div class='profile-orders-panel-title-container'>
-    <a class='profile-orders-panel-title' href='index.php?controller=Perfil&action=mostrarPedidosUsuario'>Mis pedidos</a>
-    <div class='orange-line'></div>
-</div>";
-
-echo "<table class='profile-orders-panel-page-table'>
-    <tr>
-        <th>ID Articulo</th>
-        <th>ISBN</th>
-        <th>Imagen</th>
-        <th>Nombre</th>
-        <th>Cantidad</th>
-        <th>Precio Unitario (€)</th>
-        <th>Precio Total (€)</th>
-    </tr>";
-foreach ($userOrders as $orderDetail) {
-    echo " <tr>
-        <td class='text'>" . $orderDetail['fk_id_articulo'] . "</td>
-        <td class='text'>" . $orderDetail['isbn'] . "</td>
-        <td><img class='libroicon' src='" . $orderDetail['imagen'] . "'></td>
-        <td class='text'>" . $orderDetail['nombre'] . "</td>
-        <td class='text'>" . $orderDetail['cantidad'] . "</td>
-        <td class='text'>" . formatarPrecio($orderDetail['precio_venta']) . "€</td>
-        <td class='text'>" . formatarPrecio($orderDetail['precio']) . "€</td>
-
-        </tr>";
-}
-echo "</table>
+echo "<h2 class='my-profile-title'>Detalles del pedido</h2>";
+echo "<div class='orange-line-profile'></div>";
+echo "<div class='profile-orders-panel-content-container'>
+    <div class='profile-orders-main-container'>";
+        foreach ($userOrders as $orderDetail) {
+            echo "<div class='profile-order-details-container'>
+                <p>ID Articulo: " . $orderDetail['fk_id_articulo'] . "</p>
+                <p>ISBN: " . $orderDetail['isbn'] . " </p>
+                <img src='" . $orderDetail['imagen'] . "'>
+                <p class='title'>Titulo: " . $orderDetail['nombre'] . "</p>
+                <p>Unidades: " . $orderDetail['cantidad'] . "</p>
+                <p>Precio/uds: " . formatarPrecio($orderDetail['precio_venta']) . "€</p>
+                <p>Total €: " . formatarPrecio($orderDetail['precio']) . "€</p>
+            </div>";
+        }
+    echo "</div>
 </div>";
